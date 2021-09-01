fc655eafe1ff8e4158dbc604ccbbe3f1b75e6e8c
Mikael Lagasse <2068410@cstj.qc.ca>
2021-09-01 11:21:05 -0400

    Fin du 3ieme cours ( j'pense )

--- Semaine 2/src/app.js
+++ Semaine 2/src/app.js
@@ -1,25 +1,57 @@
 import express from 'express';
+import dayjs from 'dayjs';
 
 const app = express();
 const OK = 200;
 
-// lol.
-app.get('/premiere', (request,reponse) => {
+app.get('/premiere', (request, reponse) => {
     reponse.status(OK);
     reponse.set('Content-Type', 'text/plain')
     reponse.send(`Bonjour mon deuxieme serveur\nVous utilisez presentement`)
     console.log(request)
 })
 
-app.get('/math/somme', (request,reponse) =>
-{
-    const a = request.query.a != undefined ? parseInt(request.query.a,10) : 0;
-    const b = request.query.b != undefined ? parseInt(request.query.b,10) : 0;
-    let somme = a + b;
+app.get('/math/somme', (request, reponse) => {
 
+    const a = request.query.a != undefined ? parseInt(request.query.a, 10) : 0;
+    const b = request.query.b != undefined ? parseInt(request.query.b, 10) : 0;
+    let total = 0;
+
+    const c = request.query.c;
+
+
+    switch (c) {
+        case "somme":
+            total = a + b;
+            break;
+        case "difference":
+            total = a - b;
+            break;
+        case "produit":
+            total = a * b;
+            break;
+        case "quotient":
+            total = a / b;
+            break;
+        case "reste":
+            total = a % b;
+            break;
+        default:
+            console.log("Operation non definie!")
+            return reponse.status(400).end();
+            break;
+    }
+    
     reponse.status(OK);
     reponse.set('Content-Type', 'text/html')
-    reponse.send(`<div style="background-color:${somme > 50 ? (somme > 75 ? "red" : "yellow") : "green"};font-size:800px;text-align:center;">${somme.toString()}</div>`)
+    reponse.send(`<div style="background-color:${total > 50 ? (total > 75 ? "red" : "yellow") : "green"};font-size:800px;text-align:center;">${total.toString()}</div>`)
+    
+})
+
+app.get('/date', (request, reponse) => {
+    reponse.status(OK);
+    reponse.set('Content-Type', 'text/html')
+    reponse.send(`<div style="font-size:250px;text-align:center;padding-top:150px;">${dayjs().format('YYYY-MM-DD HH:mm:ss')}</div>`)
 })
 
 export default app
